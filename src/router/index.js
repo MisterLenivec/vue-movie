3ef3c1a460a5db18416cee44c4aef7bfdf5dd562
lenivec <wormsom@gmail.com>
2020-07-31 17:09:47 +0300

    movie reviews, while without adding responses to reviews

--- src/router/index.js
+++ src/router/index.js
@@ -5,31 +5,31 @@ import Single from "../views/Single";
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/:id',
-    name: 'Single',
-    component: Single,
-    props: true
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/:id',
+        name: 'Single',
+        component: Single,
+        props: true
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+    mode: 'history',
+    routes
 })
 
 export default router
